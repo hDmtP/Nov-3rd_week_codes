93d99f551e064642626d69599869783b383aff80
hDmtP <pritamdhara914@gmail.com>
2021-11-20 13:39:07 +0530

    trying to automate score.md through .py

---
+++ my_stats.py
@@ -0,0 +1,12 @@
+from github_contributions import GithubUser
+import datetime
+
+user = GithubUser('hdmtp')
+contribs = user.contributions()
+
+contribs_2021 = user.contributions(start_date='2021-11-14', end_date=str(datetime.date.today()))
+#print(sum([day.count for day in contribs_2021.days]))
+
+f = open("score.md", "w")
+f.write(f"Score : {sum([day.count for day in contribs_2021.days])}")
+f.close()
